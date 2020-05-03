967d1105cc9e1646d569f2a579e4b04f44fee02f
metinozkn <metin.ozkn@hotmail.com>
2020-05-03 12:44:04 +0300

    added search, AddServiceModal for personnelService modal

--- src/Pages/Personnel/components/PersonnelServices.js
+++ src/Pages/Personnel/components/PersonnelServices.js
@@ -9,10 +9,13 @@ import {
   TableRow,
   Table,
   IconButton,
+  TextField,
 } from "@material-ui/core";
 
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Paper from "@material-ui/core/Paper";
+import { AddServiceModal } from "./AddServiceModal";
 
 const useStyles = makeStyles({
   table: {
@@ -55,33 +58,76 @@ export const PersonnelServices = () => {
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <Table
-        classserviceName={classes.table}
-        size="small"
-        aria-label="a dense table"
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        minWidth: "450px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: ".5em",
+          width: "100%",
+          background: "rgba(226, 226, 226, 0.58)",
+        }}
       >
-        <TableHead>
-          <TableRow>
-            <TableCell>Hizmet Adı</TableCell>
-            <TableCell align="right">Süresi</TableCell>
-            <TableCell align="right">Ücret</TableCell>
-            <TableCell align="right">Sil</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.serviceName}>
-              <TableCell component="th" scope="row">
-                {row.serviceName}
-              </TableCell>
-              <TableCell align="right">{row.time}</TableCell>
-              <TableCell align="right">{row.price}</TableCell>
-              <TableCell align="right">{row.deleteButton}</TableCell>
+        <Autocomplete
+          freeSolo
+          style={{ width: "50%" }}
+          id="free-solo-2-demo"
+          disableClearable
+          options={["saç yıkama", "saç kesim", "sakal", "ense"]}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              placeholder="Ara"
+              margin="dense"
+              variant="outlined"
+              InputProps={{ ...params.InputProps, type: "search" }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          )}
+        />
+        <AddServiceModal></AddServiceModal>
+      </div>
+      <TableContainer component={Paper}>
+        <Table
+          classserviceName={classes.table}
+          size="medium"
+          aria-label="a dense table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Hizmet Adı</TableCell>
+              <TableCell align="right">Süresi</TableCell>
+              <TableCell align="right">Ücret</TableCell>
+              <TableCell align="right">Sil</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.serviceName}>
+                <TableCell component="th" scope="row">
+                  {row.serviceName}
+                </TableCell>
+                <TableCell align="right">{row.time}</TableCell>
+                <TableCell align="right">{row.price}</TableCell>
+                <TableCell align="right">{row.deleteButton}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
